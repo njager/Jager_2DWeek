f207892447183c0e3841658f952075c98fd6762c
njager <nicholasejager@gmail.com>
2020-10-27 13:56:47 -0400

    added variables and new function

--- Jager_2DWeek/Assets/PlayerMovement.cs
+++ Jager_2DWeek/Assets/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //member variables
+    Rigidbody2D rB2D;
+
+    //public variables
+    public float runSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
     }
 }
