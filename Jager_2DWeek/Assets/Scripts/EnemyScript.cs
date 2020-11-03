d265e240f4cd0cc55d9ad48fb0d41d992a3b5b82
njager <nicholasejager@gmail.com>
2020-11-03 16:50:52 -0500

    added condiitonal statements to enemy

--- Jager_2DWeek/Assets/Scripts/EnemyScript.cs
+++ Jager_2DWeek/Assets/Scripts/EnemyScript.cs
@@ -9,11 +9,15 @@ public class EnemyScript : MonoBehaviour
 
     //member variables
     Rigidbody2D eRB;
+    bool walkingR;
+    bool walkingL;
 
     // Start is called before the first frame update
     void Start()
     {
         eRB = GetComponent<Rigidbody2D>();
+
+        walkingR = true;
     }
 
     // Update is called once per frame
@@ -25,5 +29,23 @@ public class EnemyScript : MonoBehaviour
     private void FixedUpdate()
     {
         eRB.velocity = new Vector2(enemySpeed * Time.deltaTime, eRB.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Sign"))
+        {
+            if (walkingR == true)
+            {
+                walkingL = true;
+                walkingR = false;
+            }
+            else
+            {
+                walkingL = false;
+                walkingR = true;
+            }
+        }
+        
     }
 }
