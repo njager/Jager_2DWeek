e91249cfa7ff18fe4f6d75c315d867d25ee3fc6d
njager <nicholasejager@gmail.com>
2020-11-03 16:41:30 -0500

    added movement to enemy

--- Jager_2DWeek/Assets/Scripts/EnemyScript.cs
+++ Jager_2DWeek/Assets/Scripts/EnemyScript.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    //public variables
+    public float enemySpeed;
+
+    //member variables
+    Rigidbody2D eRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        eRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        eRB.velocity = new Vector2(enemySpeed * Time.deltaTime, eRB.velocity.y);
     }
 }
